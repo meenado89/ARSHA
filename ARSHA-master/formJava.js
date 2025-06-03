47c818da61d7cf36080b50e699f1bd43eeb3f51e
Meenakshi <meenakshiemail89@gmail.com>
2025-06-02 21:42:49 -1200

    events

---
+++ ARSHA-master/formJava.js
@@ -0,0 +1,20 @@
+function handleSubmit() {
+  let email = document.getElementById("staticEmail2").value
+  let pass = document.getElementById("inputPassword2").value
+  let branch = document.getElementById("inputState").value
+
+}
+
+
+function handleChange() {
+  let branch = document.getElementById("inputState").value
+  document.getElementById("demo").innerHTML = "you are from" + branch
+}
+// function onclick(){
+//   var img=document.getElementById("first")
+//   var img=document.getElementById("first").style.backgroundImage=URL("product-3.jpg")
+
+//  }
+
+
+
