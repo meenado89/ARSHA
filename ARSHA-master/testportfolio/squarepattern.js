4e173401929f1e65ab92963d0a9cc3fd96d566b2
meenakshi <meenakshiemail89@gmail.com>
2025-06-09 14:24:20 +0530

    pattern

---
+++ ARSHA-master/testportfolio/squarepattern.js
@@ -0,0 +1,20 @@
+for(let i=0;i<4;i++){
+    for(let j=0;j<4;j++){
+        process.stdout.write("*")
+    }
+    console.log()
+}
+
+for(let i=0;i<6;i++){
+    for(let j=0;j<4;j++){
+        process.stdout.write("m")
+    }
+    console.log()
+}
+
+for(let a=0;a<3;a++){
+    for(let b=0;b<8;b++){
+        process.stdout.write("A")
+    }
+    console.log()
+}
